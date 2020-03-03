b037bf51eb329d3505e278219555997e22d6fa81
Gnome-ls <56977054+Gnome-ls@users.noreply.github.com>
2020-03-03 14:59:30 -0800

    Comentarios
    com

--- backup-base-mysql.sh
+++ backup-base-mysql.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
-# Credenciales de la base de datos
+
+# Credenciales de la base de datos productiva
 user="root"
 password="admin.SEAGED.2017"
+
+# Nombre de la base de datos
 db_name="victimas_prod"
+
+#Rutas donde se almacenarán los backups
 Diario="/home/usuario/Backups/BD/Diario"
 Semanal="/home/usuario/Backups/BD/Semanal"
 Mensual="/home/usuario/Backups/BD/Mensual"
 Anual="/home/usuario/Backups/BD/Anual"
+
+# Formato de fecha
 date=$(date +"%d-%b-%Y")
-AYER=$(date --date "yesterday" +%d-%n-%Y)
-# Set default file permissions
+
+##### variables locales #####
+# Credenciales de la base de datos
+#user="root"
+#password="Eamm#931122"
+# Nombre de la base de datos
+#db_name="Solvetic"
+# Carpeta de almacenado para backup diarios
+#Diario="/home/jccv/Backup/diario"
+# Carpeta de almacenado para backup semanales
+#Semanal="/home/jccv/Backup/semanal"
+# Carpeta de almacenado para backup mensuales
+#Mensual="/home/jccv/Backup/mensual"
+# Carpeta de almacenado para backup anuales
+#Anual="/home/jccv/Backup/anual"
+
+
+# permisos por default 
 umask 177
 
-function carpeta_log () {
+#Creacion de carpeta log
+function carpeta_log_diario () {
 
 if [ -d "$Diario/log" ]; then
     echo "Existe"
@@ -21,12 +45,20 @@ else
     mkdir log
 fi
 
+}
+
+function carpeta_log_semanal () {
+
 if [ -d "$Semanal/log" ]; then
     echo "Existe"
 else 
     cd $Semanal
     mkdir log
 fi
+
+}
+
+function carpeta_log_mensual () {
 
 if [ -d "$Mensual/log" ]; then
     echo "Existe"
@@ -35,6 +67,10 @@ else
     mkdir log
 fi
 
+} 
+
+function carpeta_log_anual () {
+
 if [ -d "$Anual/log" ]; then
     echo "Existe"
 else 
@@ -42,24 +78,30 @@ else
     mkdir log
 fi
 }
-
+# Caso 1 diario
 case $1 in
     Diario)
         echo "Iniciando backup Diario $db_name"
-        # Dump database into SQL file
+        # Dump de base de datos en archivo SQL, se almacenara en Diario con el nombre de la base-fecha
         mysqldump --user=$user -p --password=$password $db_name >$Diario/$db_name-$date.sql
         # Eliminar archivos que tengan mas de 7 dias
         cd $Diario && find $db_name* -mtime +7 -type f -exec rm {} \; 
-        #find $Diario -mtime +7 -exec rm {} \;
-        #DB backup log
-        carpeta_log;
+        # Llamado de funcion de creacion de carpeta log
+        carpeta_log_diario;
+	# Generacion de archivo.log
+	touch DB_Backup.log  
+	# Hacemos un if para preguntar si el dump existe para despues mostrar el mensaje en el archivo.log
+	# En caso de no generarse, se generara un archivo.error mostrando el mensaje que no se genero
         if [ -f "$db_name-$date.sql" ] ; then 
-                echo -e "$(date +'%d-%b-%y  %r '):ALERT:Backup de la base diaria Generada"    >>DB_Backup.log
+                echo -e "$(date +'%d-%b-%y  %r '):ALERT: Backup de la base diaria Generada"    >>DB_Backup.log
+		        # Peso maximo del archivo.log
                 MaxFileSize=2048
                 while true
                 do
-                    #sh /home/jccv/base.sh >> DB_Backup.log
+		            # Obtener el peso en bytes del archivo.log
                     file_size=`du -b DB_Backup.log | tr -s '\t' ' ' | cut -d' ' -f1`
+		            # Preguntamos si el archivo.log es mas grande en peso que MaxFileSize, movera el archivo.log 
+		            # a la carpeta log añadiendo la variable timestamp
                     if [ $file_size -gt $MaxFileSize ];then   
                         timestamp=`date +%s`
                         mv DB_Backup.log $Diario/log/DB_Backup.log.$timestamp
@@ -67,24 +109,31 @@ case $1 in
                     fi
                 done
             else 
-                echo -e "$(date +'%d-%b-%y  %r '):ALERT:Backup de la base diaria NO Generada"    >>DB_Backup.error
+                echo -e "$(date +'%d-%b-%y  %r '):ALERT: Backup de la base diaria NO Generada"    >>DB_Backup.error
         fi 
     ;;
     Semanal)
         echo "Iniciando backup Semanal $db_name"
-        # Dump database into SQL file
+        # Dump de base de datos en archivo SQL, se almacenara en Diario con el nombre de la base-fecha
         mysqldump --user=$user -p --password=$password $db_name >$Semanal/$db_name-$date.sql
         # Eliminar archivos que tengan mas de 30  dias
         cd $Semanal && find $db_name* -mtime +30 -type f -exec rm {} \;
-        #DB backup log
-        carpeta_log;
+        # Llamado de funcion de creacion de carpeta log
+        carpeta_log_semanal;
+	# Generacion de archivo.log
+	touch DB_Backup.log
+	# Hacemos un if para preguntar si el dump existe para despues mostrar el mensaje en el archivo.log
+	# En caso de no generarse, se generara un archivo.error mostrando el mensaje que no se genero
         if [ -f "$db_name-$date.sql" ] ; then 
-                echo -e "$(date +'%d-%b-%y  %r '):ALERT:Backup de la base semanal Generada"    >>DB_Backup.log
+                echo -e "$(date +'%d-%b-%y  %r '):ALERT: Backup de la base semanal Generada"    >>DB_Backup.log
+		        # Peso maximo del archivo.log
                 MaxFileSize=2048
                 while true
                 do
-                    #sh /home/jccv/base.sh >> DB_Backup.log
+                    # Obtener el peso en bytes del archivo.log
                     file_size=`du -b DB_Backup.log | tr -s '\t' ' ' | cut -d' ' -f1`
+		            # Preguntamos si el archivo.log es mas grande en peso que MaxFileSize, movera el archivo.log 
+		            # a la carpeta log añadiendo la variable timestamp
                     if [ $file_size -gt $MaxFileSize ];then   
                         timestamp=`date +%s`
                         mv DB_Backup.log $Semanal/log/DB_Backup.log.$timestamp
@@ -92,24 +141,31 @@ case $1 in
                     fi
                 done
             else 
-                echo -e "$(date +'%d-%b-%y  %r '):ALERT:Backup de la base semanal NO Generada"    >>DB_Backup.error
+                echo -e "$(date +'%d-%b-%y  %r '):ALERT: Backup de la base semanal NO Generada"    >>DB_Backup.error
         fi 
     ;;
     Mensual)
         echo "Iniciando backup Mensual $db_name"
-        # Dump database into SQL file
+        # Dump de base de datos en archivo SQL, se almacenara en Diario con el nombre de la base-fecha
         mysqldump --user=$user -p --password=$password $db_name >$Mensual/$db_name-$date.sql
         # Eliminar archivos que tengan mas de 365 dias
         cd $Mensual && find $db_name* -mtime +365 -type f -exec rm {} \;
-        #DB backup log
-        carpeta_log;
+        # Llamado de funcion de creacion de carpeta log
+        carpeta_log_mensual;
+	# Generacion de archivo.log
+	touch DB_Backup.log
+	# Hacemos un if para preguntar si el dump existe para despues mostrar el mensaje en el archivo.log
+	# En caso de no generarse, se generara un archivo.error mostrando el mensaje que no se genero
         if [ -f "$db_name-$date.sql" ] ; then 
-                echo -e "$(date +'%d-%b-%y  %r '):ALERT:Backup de la base mensual Generada"    >>DB_Backup.log
+                echo -e "$(date +'%d-%b-%y  %r '):ALERT: Backup de la base mensual Generada"    >>DB_Backup.log
+		        # Peso maximo del archivo.log
                 MaxFileSize=2048
                 while true
                 do
-                    #sh /home/jccv/base.sh >> DB_Backup.log
+		            # Obtener el peso en bytes del archivo.log
                     file_size=`du -b DB_Backup.log | tr -s '\t' ' ' | cut -d' ' -f1`
+		            # Preguntamos si el archivo.log es mas grande en peso que MaxFileSize, movera el archivo.log 
+		            # a la carpeta log añadiendo la variable timestamp
                     if [ $file_size -gt $MaxFileSize ];then   
                         timestamp=`date +%s`
                         mv DB_Backup.log $Mensual/log/DB_Backup.log.$timestamp
@@ -117,7 +173,7 @@ case $1 in
                     fi
                 done
             else 
-                echo -e "$(date +'%d-%b-%y  %r '):ALERT:Backup de la base mensual NO Generada"    >>DB_Backup.error
+                echo -e "$(date +'%d-%b-%y  %r '):ALERT: Backup de la base mensual NO Generada"    >>DB_Backup.error
         fi 
     ;;
     Anual)
@@ -126,15 +182,22 @@ case $1 in
         mysqldump --user=$user -p --password=$password $db_name >$Anual/$db_name-$date.sql
         # Eliminar archivos que tengan mas de  dias
         cd $Anual && find $db_name* -mtime +365 -type f -exec rm {} \;
-        #DB backup log
-        carpeta_log;
+        # Llamado de funcion de creacion de carpeta log
+        carpeta_log_anual;
+	# Generacion de archivo.log
+	touch DB_Backup.log
+	# Hacemos un if para preguntar si el dump existe para despues mostrar el mensaje en el archivo.log
+	# En caso de no generarse, se generara un archivo.error mostrando el mensaje que no se genero
         if [ -f "$db_name-$date.sql" ] ; then 
-                echo -e "$(date +'%d-%b-%y  %r '):ALERT:Backup de la base anual Generada"    >>DB_Backup.log
+                echo -e "$(date +'%d-%b-%y  %r '):ALERT: Backup de la base anual Generada"    >>DB_Backup.log
+		# Peso maximo del archivo.log
                 MaxFileSize=2048
                 while true
                 do
-                    #sh /home/jccv/base.sh >> DB_Backup.log
+                    # Obtener el peso en bytes del archivo.log
                     file_size=`du -b DB_Backup.log | tr -s '\t' ' ' | cut -d' ' -f1`
+		            # Preguntamos si el archivo.log es mas grande en peso que MaxFileSize, movera el archivo.log 
+		            # a la carpeta log añadiendo la variable timestamp
                     if [ $file_size -gt $MaxFileSize ];then   
                         timestamp=`date +%s`
                         mv DB_Backup.log $Anual/log/DB_Backup.log.$timestamp
@@ -142,7 +205,7 @@ case $1 in
                     fi
                 done
             else 
-                echo -e "$(date +'%d-%b-%y  %r '):ALERT:Backup de la base anual NO Generada"    >>DB_Backup.error
+                echo -e "$(date +'%d-%b-%y  %r '):ALERT: Backup de la base anual NO Generada"    >>DB_Backup.error
         fi 
     ;;
 esac
